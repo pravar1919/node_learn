8cd5ee7cf7302ed518ea8729d7c9dc08b4df14dc
pravar18 <pravar.sharma@gmail.com>
2023-11-03 23:14:29 +0530

    built-in middlewares

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
 // Middleware
 app.use(log);
 
